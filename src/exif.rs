d19ed8325bdc3c86376b6ca2fc2d4552b727772b
Alexi Peck <alexinicolaspeck@gmail.com>
2022-03-27 13:58:53 +1100

    Switch String error to Enum named errors wrapping around a string

--- src/exif.rs
+++ src/exif.rs
@@ -1,25 +1,60 @@
 use std::{
     collections::{HashMap, HashSet},
+    fmt,
     path::Path,
     process::{Command, Stdio},
     sync::Arc,
-    time::Duration,
 };
 
-pub const EXIT_DELAY: Duration = Duration::from_millis(250);
+type List = Arc<HashSet<String>>;
+
+pub enum ExifError {
+    FileNotFound(String),
+    TagError(String),
+    ValueError(String),
+    ExifToolError(String),
+    FromUtf8Error(String),
+    UncontrolledError(String),
+}
+
+impl fmt::Display for ExifError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::FileNotFound(error_message) => format!("FileNotFound: {}", error_message),
+                Self::TagError(error_message) => format!("TagError: {}", error_message),
+                Self::ValueError(error_message) => format!("ValueError: {}", error_message),
+                Self::ExifToolError(error_message) => format!("ExifToolError: {}", error_message),
+                Self::FromUtf8Error(error_message) => format!("FromUtf8Error: {}", error_message),
+                Self::UncontrolledError(error_message) =>
+                    format!("UncontrolledError: {}", error_message),
+            }
+        )
+    }
+}
+
+pub fn create_list_from_vec(tag_list: Vec<&str>) -> List {
+    let mut list: HashSet<String> = HashSet::new();
+    for element in tag_list {
+        list.insert(element.to_string());
+    }
+    Arc::new(list)
+}
 
 pub fn exiftool_available() -> bool {
-    return !Command::new("exiftool")
+    return Command::new("exiftool")
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn()
-        .is_err();
+        .is_ok();
 }
 
 pub enum Mode {
     All,
-    Whitelist(Arc<HashSet<String>>),
-    Blacklist(Arc<HashSet<String>>),
+    Whitelist(List),
+    Blacklist(List),
 }
 
 pub struct Exif {
@@ -32,33 +67,46 @@ impl Exif {
             attributes: HashMap::new(),
         }
     }
-    pub fn new(file_path: &Path, mode: Mode) -> Result<Self, String> {
+    pub fn new(file_path: &Path, mode: Mode) -> Result<Self, ExifError> {
         let mut exif = Self::default();
         let child = match Command::new("exiftool")
             .arg(file_path)
             .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .spawn()
         {
             Ok(child) => child,
             Err(err) => {
-                return Err(err.to_string());
+                return Err(ExifError::ExifToolError(err.to_string()));
             }
         };
 
         let output = match child.wait_with_output() {
             Ok(output) => output,
             Err(err) => {
-                return Err(err.to_string());
+                return Err(ExifError::ExifToolError(err.to_string()));
             }
         };
 
-        let output = match String::from_utf8(output.stdout) {
+        let stdout_output = match String::from_utf8(output.stdout) {
             Ok(output) => output,
             Err(err) => {
-                return Err(err.to_string());
+                return Err(ExifError::FromUtf8Error(err.to_string()));
             }
         };
-        let output = output.split('\n');
+
+        let stderr_output = match String::from_utf8(output.stderr) {
+            Ok(output) => output,
+            Err(err) => {
+                return Err(ExifError::FromUtf8Error(err.to_string()));
+            }
+        };
+
+        if stderr_output.contains("Error: File not found -") {
+            return Err(ExifError::FileNotFound(stderr_output.trim().to_string()));
+        }
+
+        let output = stdout_output.split('\n');
         for line in output {
             let line = String::from(line);
             let line_split = line.split(':');
@@ -69,35 +117,39 @@ impl Exif {
                     tag
                 }
                 None => {
-                    return Err("Error getting tag from Exif data".to_string());
+                    return Err(ExifError::TagError(
+                        "Error getting tag from Exif data".to_string(),
+                    ));
                 }
             };
-            if tag == "" {
+            if tag.is_empty() {
                 continue;
             }
-            
+
             match &mode {
-                Mode::All => {},
+                Mode::All => {}
                 Mode::Whitelist(list) => {
                     if list.contains(&tag) {
                         break;
                     } else {
                         continue;
                     }
-                },
+                }
                 Mode::Blacklist(list) => {
                     if !list.contains(&tag) {
                         break;
                     } else {
                         continue;
                     }
-                },
+                }
             }
 
             let value = match line_split.last() {
                 Some(value) => value.trim().to_string(),
                 None => {
-                    return Err("Error getting value from Exif data".to_string());
+                    return Err(ExifError::TagError(
+                        "Error getting value from Exif data".to_string(),
+                    ));
                 }
             };
             exif.attributes.insert(tag, value);
--- src/testing.rs
+++ src/testing.rs
@@ -1,8 +1,10 @@
 #[cfg(test)]
 mod tests {
+    use crate::exif::{create_list_from_vec, exiftool_available, Exif, Mode};
     use std::path::Path;
 
-    use crate::exif::{exiftool_available, Exif, Mode};
+    pub const TEST_FILE_PATH: &str =
+        r"C:\Users\Alexi Peck\Desktop\mapping_tool\target\release\source_one\DJI_0013.JPG";
 
     #[test]
     fn test_exiftool_available() {
@@ -12,17 +14,64 @@ mod tests {
     }
 
     #[test]
-    fn test_pull_exif_data() {
-        match Exif::new(Path::new(
-            r"C:\Users\Me\Desktop\mapping_tool\target\release\source_one\DJI_0013.JPG",
-        ), Mode::All) {
+    fn test_pull_exif_data_all() {
+        match Exif::new(Path::new(TEST_FILE_PATH), Mode::All) {
             Ok(exif) => {
                 for (tag, value) in exif.attributes.iter() {
                     println!("{}:{}", tag, value);
                 }
             }
             Err(err) => {
-                println!("{}", err);
+                panic!("{}", err);
+            }
+        };
+    }
+
+    #[test]
+    fn test_pull_exif_data_whitelist() {
+        let whitelist = create_list_from_vec(vec![
+            "GPSLatitude",
+            "GPSLongitude",
+            "GPSAltitude",
+            "ExifImageWidth",
+            "ExifImageHeight",
+            "FlightYawDegree",
+            "AbsoluteAltitude",
+            "RelativeAltitude",
+            "FieldOfView",
+            "FocalLength",
+        ]);
+
+        match Exif::new(Path::new(TEST_FILE_PATH), Mode::Whitelist(whitelist)) {
+            Ok(exif) => {
+                for (tag, value) in exif.attributes.iter() {
+                    println!("{}:{}", tag, value);
+                }
+            }
+            Err(err) => {
+                panic!("{}", err);
+            }
+        };
+    }
+
+    #[test]
+    fn test_pull_exif_data_blacklist() {
+        let blacklist = create_list_from_vec(vec![
+            "SerialNumber",
+            "FileModificationDate/Time",
+            "DigitalZoomRatio",
+            "XPComment",
+            "XPKeywords",
+        ]);
+
+        match Exif::new(Path::new(TEST_FILE_PATH), Mode::Blacklist(blacklist)) {
+            Ok(exif) => {
+                for (tag, value) in exif.attributes.iter() {
+                    println!("{}:{}", tag, value);
+                }
+            }
+            Err(err) => {
+                panic!("{}", err);
             }
         };
     }
